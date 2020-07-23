2b71c5ee0890d1b2c33f5d82491314a72bc49744
JuliaGehrlein <45388518+JuliaGehrlein@users.noreply.github.com>
2020-07-23 10:03:51 -0400

    Update analysis_code_pval_lmad_2t1e.py

--- analysis_code_pval_lmad_2t1e.py
+++ analysis_code_pval_lmad_2t1e.py
@@ -1,3 +1,4 @@
+#MC simulation for LMA Dark
 import numpy as np
 import matplotlib.pyplot  as plt
 from scipy.integrate import quad
@@ -25,14 +26,14 @@ time=308.1
 f_Cs=(A_Cs)/(A_Cs+A_I)
 f_I=(A_I)/(A_Cs+A_I)
 
-#best fit
+#best fit NSI parameter
 epseu=    2.02366521e-01
 epsmu=  -2.95015935e-02
   
 
-
+#calculate the signal using the best fit NSI parameters
 sig=csir.signalcomp_lmad(epseu,epsmu)
-#chi2=147.7320555091213
+
 #load data
 dat=acc.load_file("datafiles_in/Co_data.dat")
 #load neutrons
@@ -43,22 +44,24 @@ ACon=np.loadtxt("datafiles_in/full_AC_on.dat")
 ACoff=np.loadtxt("datafiles_in/full_AC_off.dat")
 COoff=np.loadtxt("datafiles_in/full_CO_off.dat")
 bkg1dsdata=acc.bkgreal_2ds(ACoff,ACon,COoff)
-#print( minimize(lambda x: csir.chi2_1d( n,bkg1dsdata,dat,x[0],x[1],x[2],x[3]),(0.0,0.0,0.0,0.0),method='SLSQP',tol=1e-5,options={"maxiter":1e3}))
-#print(csir.chi2_bins(n,bkg1dsdata,dat,epsee,  epsmm,epsem,epset,epsmt,0,0,0))
+
+
 
 def randompred(sig,n,bkg):
+ #if one wants to use the new pull term parametrization uncomment the next two lines
 #        alpha=stat.pulldist(0.28)
  #       beta=stat.pulldist(0.25)
-        alpha=np.random.normal(0, 0.28)
+        alpha=np.random.normal(0, 0.28)# Gaussian pull terms
         beta=np.random.normal(0, 0.25)
         sig_n=csir.add_lists_2(sig,n,alpha,beta)
+    #if smearing is required uncomment the next line
  #       smeared=acc.smearing_fct(sig_n)
         pred=acc.data_eff(sig_n)
         #add bkg                          
         lista=csir.add_lists_1(pred,bkg,0)
         return lista
 
-
+#apply Poisson fluctuations to the predicted number of events in each bin
 def randomdata(lista):
         arrout = np.zeros((2, 3))
         bins=csir.rebin_list_1E2t(lista)
@@ -93,6 +96,7 @@ def chi2_bins(n,ac,meas,epseu,epsmu,alpha,beta,gamma):
 
                 chi += add
         return 2*chi+(alpha/0.28)**2+(beta/0.25)**2+(gamma/0.06)**2
+       #if the new pull term parametrization is used uncomment the next line
   #      return 2*chi+ 2*(alpha - np.log(alpha + 1.))/0.28**2+2*(beta - np.log(beta + 1.))/0.25**2+2*(gamma - np.log(gamma + 1.))/0.06**2
 
 def mini_bins(epseu,epsmu,n,meas,bkgdata):
@@ -108,7 +112,6 @@ for i in range(nn):
         pp,outt=randomdata(random)
         if pp<10:
                 res=mini_bins(epseu,epsmu,n,outt,bkg1dsdata)
-                fileout[i,0]=res.fun
-#                print("min",res.fun)
+                fileout[i,0]=res.fun                
                 np.savetxt("datafiles_out/result_pval_nsi_lmad_2t1e_nomarg_nosmear_gauss006_5.txt",fileout)
-#print(fileout)
+
